d924ff0d9f4811ca93d04a2c20ed57fa007601a0
jeewaa <jeewa228@gmail.com>
2024-03-09 21:33:10 +0530

    created dashboard page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import SignIn from "../pages/sign-in/index.jsx";
 import SignUp from "../pages/sign-up/index.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "../pages/dashboard/index.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                     <Route exact path="/" element={<SignIn/>}/>
                     <Route exact path="/signin" element={<SignIn/>}/>
                     <Route exact path="/signup" element={<SignUp/>}/>
+                    <Route exact path="/dashboard" element={<Dashboard/>}/>
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,190 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import ViewAgendaIcon from '@mui/icons-material/ViewAgenda';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import {useState} from "react";
+import {Grid} from "@mui/material";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...(open && {
+            ...openedMixin(theme),
+            '& .MuiDrawer-paper': openedMixin(theme),
+        }),
+        ...(!open && {
+            ...closedMixin(theme),
+            '& .MuiDrawer-paper': closedMixin(theme),
+        }),
+    }),
+);
+
+function Dashboard() {
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(true);
+    const [getClick, setClick] = useState(0);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
+    const buttonClick = (props) => {
+        console.log(props);
+        setClick(props);
+    };
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="fixed" open={open}>
+                <Toolbar
+                    style={{
+                        backgroundColor: "#ff8f04"
+                    }}
+                >
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{
+                            marginRight: 5,
+                            ...(open && { display: 'none' }),
+                        }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div">
+                        Orange
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    {['Dashboard', 'Customer', 'Product', 'Order'].map((text, index) => (
+                        <ListItem key={index} disablePadding
+                                  onClick={() =>buttonClick(index)}
+                                  sx={{
+                                      display: 'block',
+                                      backgroundColor: index === getClick ? '#ff8f04' : '#ffffff',
+                                      color: index === getClick ? '#ffffff' : '#ff8f04',
+                                      margin: '6px',
+                                      borderRadius: '6px',
+                                      width: '226px',
+                        }}>
+                            <ListItemButton
+                                sx={{
+                                    minHeight: 48,
+                                    justifyContent: open ? 'initial' : 'center',
+                                    px: 2.5,
+                                }}
+                            >
+                                <ListItemIcon
+                                    sx={{
+                                        minWidth: 0,
+                                        mr: open ? 3 : 'auto',
+                                        justifyContent: 'center',
+                                        color: index === getClick ? '#ffffff' : '#ff8f04',
+                                    }}
+                                >
+                                    {
+                                        index === 0 ? <DashboardIcon/> : index === 1 ? <PeopleAltIcon/> : index === 2 ? <ViewAgendaIcon/> : <ShoppingBagIcon/>
+                                    }
+                                </ListItemIcon>
+                                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                <DrawerHeader />
+                <Grid container>
+                    <Typography>Dashboard</Typography>
+                </Grid>
+            </Box>
+        </Box>
+    );
+}
+export default Dashboard;
